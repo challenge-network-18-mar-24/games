4a6578cd52efce6a7d76e4d1b77c0c472396ac3b
Christian95Sp <spinachristian95@gmail.com>
2024-04-10 18:46:34 +0200

    test

--- src/data.js
+++ src/data.js
@@ -1,44 +1,7 @@
 const data = [
   {
-    name: "My Game - Stefano",
-    gameUrl: "",
-    codeUrl: "https://github.com/stemart89/stefano-games.git"
-
-  },
-  {
-    name: "My Game - Giulia",
-    gameUrl: "",
-    codeUrl: "https://github.com/m-giulia/my-game.git"
-  },
-  {
-    name: " My name = arnol ",
-    gameUrl: "",
-    codeUrl: "https://github.com/blonedl97/arnol.game"
-  },
-  {
-    name: " My name = arnol ",
-    gameUrl: "",
-    codeUrl: "https://github.com/blonedl97/arnol.game"
-
-  },
-  {
-    name: "My Game - AnaLitvi",
-    gameUrl: "",
-    codeUrl: "https://github.com/AnaLitvi/Ana_Litvi_Game.git"
-  },
-  {
-    name: "My Game - Alessia",
-    gameUrl: "",
-    codeUrl: "https://github.com/arshenAll/games.git"
-  },
-  {
     name: "My Game - Christian",
     gameUrl: "",
     codeUrl: "https://github.com/Christian95Sp/game_Chri"
-  },
-  {
-    name: "My Game - Alessia",
-    gameUrl: "",
-    codeUrl: "https://github.com/arshenAll/games.git"
   }
 ]
