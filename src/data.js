edbf5d2f6263bb048ce1c0e415b0e5e2c3227762
yournewgoodname <yournew@email.com>
2024-04-10 17:31:55 +0200

    aggiunta link maria pia

--- src/data.js
+++ src/data.js
@@ -8,5 +8,10 @@ const data = [
     name: "My Game - Giulia",
     gameUrl: "",
     codeUrl: "https://github.com/m-giulia/my-game.git"
+  },
+  {
+    name: "My Game - Maria Pia",
+    gameUrl: "",
+    codeUrl: "https://github.com/mariapiastella/MariaPia-game"
   }
 ]
