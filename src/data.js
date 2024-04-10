5d96b37fbe439afe525084633f171d8fee36cf4b
m-giulia <163854119+m-giulia@users.noreply.github.com>
2024-04-10 17:07:22 +0200

    giulia (#1)

--- src/data.js
+++ src/data.js
@@ -3,5 +3,10 @@ const data = [
     name: "My Game - Andrea",
     gameUrl: "",
     codeUrl: ""
+  },
+  {
+    name: "My Game - Giulia",
+    gameUrl: "",
+    codeUrl: "https://github.com/m-giulia/my-game.git"
   }
 ]
