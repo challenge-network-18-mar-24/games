b8701e71af1a20186e88fb21160b29f599fdd260
Ferro-coding <95420374+Ferro-coding@users.noreply.github.com>
2024-04-10 17:29:59 +0200

    attilio update

--- src/data.js
+++ src/data.js
@@ -1,14 +1,5 @@
 const data = [
-  {
-    name: "My Game - Andrea",
-    gameUrl: "",
-    codeUrl: ""
-  },
-  {
-    name: "My Game - Giulia",
-    gameUrl: "",
-    codeUrl: "https://github.com/m-giulia/my-game.git"
-  },
+
   {
     name: "My Game - Attilio",
     gameUrl: "",
