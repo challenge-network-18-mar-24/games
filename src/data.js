6c15b48e38569389a28490b797aa73c29956a918
Christian95Sp <spinachristian95@gmail.com>
2024-04-10 17:30:55 +0200

    aggiunta link Christian game repo

--- src/data.js
+++ src/data.js
@@ -8,5 +8,10 @@ const data = [
     name: "My Game - Giulia",
     gameUrl: "",
     codeUrl: "https://github.com/m-giulia/my-game.git"
+  },
+  {
+    name: "My Game - Christian",
+    gameUrl: "",
+    codeUrl: "https://github.com/Christian95Sp/game_Chri"
   }
 ]
