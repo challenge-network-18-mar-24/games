9eb67117426ee47524c261e2bb9d27513d77d2a3
mila20242024 <emilycoopers@hotmail.com>
2024-04-10 17:30:18 +0200

    aggiunta link milagames

--- src/data.js
+++ src/data.js
@@ -8,5 +8,10 @@ const data = [
     name: "My Game - Giulia",
     gameUrl: "",
     codeUrl: "https://github.com/m-giulia/my-game.git"
+  },
+  {
+    name: "My Game - Mila!",
+    gameUrl: "",
+    codeUrl: "https://github.com/mila20242024/milagames"
   }
 ]
