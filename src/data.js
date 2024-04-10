1e0aa0e2e2bc6966310b7bbcaa28726a483afbb8
Christian95Sp <spinachristian95@gmail.com>
2024-04-10 18:48:00 +0200

    my branch game chri repo

--- src/data.js
+++ src/data.js
@@ -1,5 +1,38 @@
 const data = [
   {
+    name: "My Game - Stefano",
+    gameUrl: "",
+    codeUrl: "https://github.com/stemart89/stefano-games.git"
+
+  },
+  {
+    name: "My Game - Giulia",
+    gameUrl: "",
+    codeUrl: "https://github.com/m-giulia/my-game.git"
+  },
+  {
+    name: "My Game - Mila!",
+    gameUrl: "",
+    codeUrl: "https://github.com/mila20242024/milagames"
+  },
+  {
+    name: " My name = arnol ",
+    gameUrl: "",
+    codeUrl: "https://github.com/blonedl97/arnol.game"
+
+  },
+
+  {
+    name: "My Game - AnaLitvi",
+    gameUrl: "",
+    codeUrl: "https://github.com/AnaLitvi/Ana_Litvi_Game.git"
+  },
+  {
+    name: "My Game - Alessia",
+    gameUrl: "",
+    codeUrl: "https://github.com/arshenAll/games.git"
+  },
+  {
     name: "My Game - Christian",
     gameUrl: "",
     codeUrl: "https://github.com/Christian95Sp/game_Chri"
