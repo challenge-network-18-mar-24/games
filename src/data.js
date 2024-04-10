262181a1b304a97e39d11e669b38f9e5d982f092
stemart89 <163853963+stemart89@users.noreply.github.com>
2024-04-10 17:45:20 +0200

    stefano (#3)

--- src/data.js
+++ src/data.js
@@ -1,4 +1,5 @@
 const data = [
+<<<<<<< HEAD
     {
       name: "My Game - Andrea",
       gameUrl: "",
@@ -10,11 +11,31 @@ const data = [
       gameUrl: "",
       codeUrl: "https://github.com/m-giulia/my-game.git"
     },
+=======
+  {
+    name: "My Game - Stefano",
+    gameUrl: "",
+    codeUrl: "https://github.com/stemart89/stefano-games.git"
+
+  },
+  {
+    name: "My Game - Giulia",
+    gameUrl: "",
+    codeUrl: "https://github.com/m-giulia/my-game.git"
+  },
+  {
+    name: " My name = arnol ",
+    gameUrl: "",
+    codeUrl: "https://github.com/blonedl97/arnol.game"
+
+  },
+>>>>>>> c007d0f (stefano (#3))
 
   {
     name: "My Game - AnaLitvi",
     gameUrl: "",
     codeUrl: "https://github.com/AnaLitvi/Ana_Litvi_Game.git"
+<<<<<<< HEAD
   },
   {
     name: "My Game - Stefano",
@@ -40,4 +61,12 @@ const data = [
       codeUrl: "https://github.com/arshenAll/games.git"
     },
 
+=======
+  }, 
+  {
+    name: "My Game - Alessia",
+    gameUrl: "",
+    codeUrl: "https://github.com/arshenAll/games.git"
+  }
+>>>>>>> c007d0f (stefano (#3))
 ]
