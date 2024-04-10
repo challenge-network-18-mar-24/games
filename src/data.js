cdb228a17d1f11a1242be67369a6349931b43598
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-04-10 17:37:58 +0200

    leo

--- src/data.js
+++ src/data.js
@@ -1,18 +1,25 @@
 const data = [
-  {
-    name: "My Game - Andrea",
-    gameUrl: "",
-    codeUrl: ""
-  },
-  {
-    name: "My Game - Giulia",
-    gameUrl: "",
-    codeUrl: "https://github.com/m-giulia/my-game.git"
-  }
+    {
+		name: "My Game - Andrea",
+		gameUrl: "",
+		codeUrl: ""
+    },
 
-  {
-    name: "My Game - AnaLitvi",
-    gameUrl: "",
-    codeUrl: "https://github.com/AnaLitvi/Ana_Litvi_Game.git"
-  }
+    {
+		name: "My Game - Giulia",
+		gameUrl: "",
+		codeUrl: "https://github.com/m-giulia/my-game.git"
+    },
+
+    {
+		name: "My Game - AnaLitvi",
+		gameUrl: "",
+		codeUrl: "https://github.com/AnaLitvi/Ana_Litvi_Game.git"
+    },
+
+    {
+		name: "My Game - Leo",
+		gameUrl: "",
+		codeUrl: "https://github.com/leonardo-pesci/leo-games.git"
+    }
 ]
