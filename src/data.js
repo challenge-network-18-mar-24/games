d853db5c1cf7b92dbc20906c55166c8018455480
Macrifabrizio99 <163854502+Macrifabrizio99@users.noreply.github.com>
2024-04-11 17:19:40 +0200

    aggiunto_fabrizio (#21)

--- src/data.js
+++ src/data.js
@@ -82,6 +82,11 @@ const data = [
     name: "My Game - Angelica",
     gameUrl: "",
     codeUrl: "https://github.com/Angycapuozzo/angelica-game.git" 
+  },
+  {
+    name: "My Game - Fabrizio",
+    gameUrl: "",
+    codeUrl: "https://github.com/Macrifabrizio99/My-game.git"
   }
 ]
 
