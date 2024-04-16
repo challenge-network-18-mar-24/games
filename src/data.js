08109079cd2ab082810e3588826e08827344fe47
arshenAll <alessia.gottardo01@gmail.com>
2024-04-16 16:15:50 +0200

    added-pages-link

--- src/data.js
+++ src/data.js
@@ -1,17 +1,97 @@
 const data = [
+
   {
-    name: "My Game - Andrea",
+    name: "My Game - Stefano",
     gameUrl: "",
-    codeUrl: ""
+    codeUrl: "https://github.com/stemart89/stefano-games.git"
+
   },
   {
     name: "My Game - Giulia",
-    gameUrl: "",
+    gameUrl: "https://m-giulia.github.io/my-game/",
     codeUrl: "https://github.com/m-giulia/my-game.git"
+  },
+  {
+    name: "My Game - Mila!",
+
+    gameUrl: "https://mila20242024.github.io/milagames/",
+    codeUrl: "https://github.com/mila20242024/milagames"},
+
+  {
+    name: " My name = arnol ",
+    gameUrl: "",
+    codeUrl: "https://github.com/blonedl97/arnol.game"
+
+  },
+
+  {
+    name: "My Game - AnaLitvi",
+    gameUrl: "",
+    codeUrl: "https://github.com/AnaLitvi/Ana_Litvi_Game.git"
+  },
+  {
+    name: "My Game - Alessia",
+    gameUrl: "https://arshenall.github.io/games/",
+    codeUrl: "https://github.com/arshenAll/games.git"
+  },
+  {
+
+    name: "My Game - Francesco",
+    gameUrl: "",
+    codeUrl: "https://github.com/GalloFrancesco01/game.git"
+  },
+  {
+    name: "My Game - Maria Pia",
+    gameUrl: "",
+    codeUrl: "https://github.com/mariapiastella/MariaPia-game"
+
+  },
+  {
+    name: "My Game - Sabrina",
+    gameUrl: "",
+    codeUrl: "https://github.com/Sabbcorr/my-game-sab-cor.git"
+
+  },
+  {
+
+    name: "My Game - Leo",
+    gameUrl: "",
+    codeUrl: "https://github.com/leonardo-pesci/leo-games.git"
+
+  },
+  {
+
+    name: "My Game - Sofia",
+
+    gameUrl: "https://sofiaabou.github.io/game/",
+    codeUrl: "https://github.com/SofiaAbou/game.git"
+
+  },
+
+
+  {
+    name: "My Game - Carmela",
+    gameUrl: "",
+    codeUrl: "https://github.com/carmela-scarda/-un-giocoo.git"
+
+  },
+  {
+    name: "My Game - Christian",
+
+    gameUrl: "https://christian95sp.github.io/game_Chri/",
+    codeUrl: "https://github.com/Christian95Sp/game_Chri"
+  },
+  {
+    name: "My Game - Angelica",
+    gameUrl: "https://angycapuozzo.github.io/angelica-game/",
+    codeUrl: "https://github.com/Angycapuozzo/angelica-game.git" 
+  },
+  {
+    name: "My Game - Fabrizio",
+    gameUrl: "",
+    codeUrl: "https://github.com/Macrifabrizio99/My-game.git"
   }
-,{
-  name: "My Game - Alessia",
-  gameUrl: "https://arshenall.github.io/games/",
-  codeUrl: "https://github.com/arshenAll/games.git"
-}
 ]
+
+
+
