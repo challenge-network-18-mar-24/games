627282cb7bb791de372a7af4ad509b6bb1d16f15
m-giulia <163854119+m-giulia@users.noreply.github.com>
2024-04-16 10:55:30 +0200

    Giulia (#5)
    * giulia
    
    * url game aggiunto

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ const data = [
   },
   {
     name: "My Game - Giulia",
-    gameUrl: "",
+    gameUrl: "https://m-giulia.github.io/my-game/",
     codeUrl: "https://github.com/m-giulia/my-game.git"
   },
   {
