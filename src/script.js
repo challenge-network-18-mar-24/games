5168877ae9a2a6b8e63a13111e9a8aa13ebbe58a
andreamagazzini <andreamaga4@gmail.com>
2024-04-16 09:57:58 +0200

    add carousel logic

--- src/script.js
+++ src/script.js
@@ -1,8 +1,33 @@
-const container = document.getElementById("container");
+const container = document.getElementById("carousel");
+
+const previousButton = document.getElementById("previousButton");
+const nextButton = document.getElementById("nextButton");
+
 const link = document.createElement("a");
 
-data.forEach((object) => {
-  link.href = object.gameUrl;
-  link.innerText = object.name;
+let currentIndex = 0;
+
+/*
+* La funzione ha un argomento next che sarà 
+* -1 se cliccato previousButton
+* 1 se cliccato nextButton
+*/
+const changeGame = (next) => {
+  currentIndex = currentIndex + next;
+
+  // Disabilita previousButton per il primo elemento
+  if(currentIndex === 0) {
+    previousButton.disabled = true;
+  }
+
+  // Disabilita nextButton per l'ultimo elemento
+  if(currentIndex === data-length - 1) {
+    nextButton.disabled = true;
+  }
+
+  link.href = data[currentIndex].gameUrl;
+  link.innerText = data[currentIndex].name;
   container.appendChild(link);
-})
+}
+
+changeGame(0);
